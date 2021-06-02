46cf19d66000413c3a05483e8686d38e4bf037de
Abdurauf Sherkulov <abduraufsherkulov@gmail.com>
2021-06-02 12:50:32 +0500

    features modified

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -1,31 +1,34 @@
 /**
- * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
-import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import List from '@ckeditor/ckeditor5-list/src/list.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+
+// The editor creator to use.
+import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount';
 
-class Editor extends ClassicEditor {}
+export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
-Editor.builtinPlugins = [
+ClassicEditor.builtinPlugins = [
+	Essentials,
 	Autoformat,
 	Bold,
-	Essentials,
+	Italic,
 	Heading,
 	Indent,
-	Italic,
 	Link,
 	List,
 	Paragraph,
